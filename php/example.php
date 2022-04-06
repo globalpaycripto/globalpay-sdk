5f16160e1ac746560b14451c2e6d7d682e726497
globalpaycripto <102989001+globalpaycripto@users.noreply.github.com>
2022-04-06 12:13:16 -0500

    Update example.php

--- php/example.php
+++ php/example.php
@@ -9,16 +9,16 @@ $globalpay = new GlobalpayAPI($privateKey, $token);
 try {
 
     //$balance = $globalpay->GetBalance();
-    //print_r ($balance);
+    //print_r (json_encode($balance,JSON_PRETTY_PRINT,512));
 
-    $rates = $globalpay->GetRates();
-    print_r($rates);
+    //$rates = $globalpay->GetRates();
+    //print_r (json_encode($rates,JSON_PRETTY_PRINT,512));
 
-    //$transactions = $globalpay->GetTransactions('2022-01-01','2022-12-31');
-    //print_r ($transactions);
+    //$transactions = $globalpay->GetTransactions('2022-01-01','2022-12-31','BTC','CPGD36JZDOAGIRWEF0TRETPJDU');
+    //print_r (json_encode($transactions,JSON_PRETTY_PRINT,512));
 
-    //$transaction = $globalpay->CreateTransaction(0.0001, 'BTC', 'AABBCCDDEE', 'https://mysite.com/transactions/end', 'user@mysite.com');
-    //print_r ($transaction);
+    $transaction = $globalpay->CreateTransaction(0.04447, 'LTCT', 'AABBCCDDEE', 'https://mysite.com/transactions/end', 'user@mysite.com');
+    print_r (json_encode($transaction,JSON_PRETTY_PRINT,512));
 
 } catch (Exception $e) {
     print_r($e);
